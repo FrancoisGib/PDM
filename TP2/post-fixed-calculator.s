d960e69882a7b1d5f485fb95985e009cb1b81d6b
François Gibier <francois.gibier.etu@univ-lille.fr>
2024-02-16 15:40:40 +0100

    le poulet est fabuleux le masque j'ai jamais vu le qi

--- TP2/post-fixed-calculator.s
+++ TP2/post-fixed-calculator.s
@@ -15,6 +15,65 @@ space:
 str_to_num:
     .byte 48
 .text
+
+.globl addition
+.type addition, @function
+addition:
+    push %rbp /* sauvegarde pointeur base */
+    movq %rsp, %rbp
+    push %rbx /* sauvegarde des registres */
+    movq 16(%rbp), %r8
+    movq 24(%rbp), %r9
+    addq %r8, %r9
+    popq %rbx /* on restaure les registres */
+    movq %rbp, %rsp /* restauration pointeur base */
+    popq %rbp
+    ret
+
+.globl soustraction
+.type soustraction, @function
+soustraction:
+    push %rbp /* sauvegarde pointeur base */
+    movq %rsp, %rbp
+    push %rbx /* sauvegarde des registres */
+    movq 16(%rbp), %r8
+    movq 24(%rbp), %r9
+    subq %r8, %r9 /* reste stocké dans r8 et ici on doit inverser les opérandes */
+    movq %r9, %rax
+    popq %rbx /* on restaure les registres */
+    movq %rbp, %rsp /* restauration pointeur base */
+    popq %rbp
+    ret
+   
+.globl multiplication
+.type multiplication, @function
+multiplication:
+    push %rbp /* sauvegarde pointeur base */
+    movq %rsp, %rbp
+    push %rbx /* sauvegarde des registres */
+    movq 16(%rbp), %r8
+    movq 24(%rbp), %r9
+    imul %r8, %r9
+    popq %rbx /* on restaure les registres */
+    movq %rbp, %rsp /* restauration pointeur base */
+    popq %rbp
+    ret    
+
+.globl division
+.type division, @function
+division:
+    push %rbp /* sauvegarde pointeur base */
+    movq %rsp, %rbp
+    push %rbx /* sauvegarde des registres */
+    movq 16(%rbp), %rbx
+    movq 24(%rbp), %rax
+    idiv %ebx /* resultat dans rax */
+    movq %rax, %r9
+    popq %rbx /* on restaure les registres */
+    movq %rbp, %rsp /* restauration pointeur base */
+    popq %rbp
+    ret
+
 .globl _start 
 _start: 
     movq $0, %rax
@@ -26,10 +85,6 @@ _start:
     movq $operators, %r13
     movq $res, %r11
     movq $0, %rdx
-
-# cas 1 nombre
-# cas 2 operateur
-# cas 3 fin
 
 read:
     movq $0, %r10
@@ -52,46 +107,50 @@ read_number:
     movb (%r14), %r10b
     cmpb str_to_num, %r10b /* de base je faisait que pour les espaces mais c'est mieux juste de vérifier que ce n'est pas un chiffre car on peut enlever
                             maintenant enlever les espaces */
-    jl push
+    jl push_number
     inc %r14
     jmp read_number
 
-push:
+push_number:
     pushq %r9
     jmp read
 
+neg:
+    movb $'-', (%r11)
+    movq $1, %r8
+    inc %r11
+    inc %r12
+    movq $4294967296, %r10
+    subq $1, %r10
+    subq $1, %rax
+    xor %r10, %rax
+    jmp init
+
 do_operation: /* depile les deux nombres et jump */
-    popq %r9
-    popq %r8
     cmpb (%r13), %r10b
     je add
     cmpb 1(%r13), %r10b
     je sub
     cmpb 2(%r13), %r10b
-    je mult
+    je mul
     cmpb 3(%r13), %r10b
     je div
 
 add:
-    addq %r8, %r9
-    jmp push
+    call addition
+    jmp push_number
 
 sub:
-    subw %r9w, %r8w /* reste stocké dans r8 et ici on doit inverser les opérandes */
-    movw %r8w, %r9w
-    jmp push
+    call soustraction
+    jmp push_number
 
-mult:
-    imulw %r8w, %r9w
-    jmp push
+mul:
+    call multiplication
+    jmp push_number
 
 div:
-    movq $0, %rdx
-    movq %r8, %rax
-    movq %r9, %rbx
-    idiv %ebx /* resultat dans rax */
-    movq %rax, %r9
-    jmp push
+    call division
+    jmp push_number
 
 result_equals_zero:
     movq $'0', (%r11)
@@ -102,22 +161,15 @@ result_equals_zero:
 
 init_cpt:
     popq %rax
+    cmpq $0, %rax
+    jl neg
+
+init:
     movq $0, %r13
     movq $buffer, %r15
-    cmpq $32767, %rax
-    jg neg
     cmpl $0, %eax
     je result_equals_zero
     jmp convert_result_to_string
-
-neg:
-    movb $'-', (%r11)
-    movq $1, %r8
-    inc %r11
-    inc %r12
-    movq $65536, %r8
-    subq %rax, %r8 /* reste stocké dans r8 et ici on doit inverser les opérandes */
-    movq %r8, %rax
     
 
 convert_result_to_string:
@@ -160,3 +212,5 @@ done:
     movq $60,%rax 
     xor %rdi,%rdi     
     syscall     
+
+
